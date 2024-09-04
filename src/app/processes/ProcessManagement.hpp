9d2d28bae0803a0cf80904aae0b935834386e086
Kunal Mali <malikunal2202@gmail.com>
2024-09-04 22:32:12 +0530

    Adding shared memory construct and implementing Semaphores, Locks

--- src/app/processes/ProcessManagement.hpp
+++ src/app/processes/ProcessManagement.hpp
@@ -2,18 +2,40 @@
 #define PROCESS_MANAGEMENT_HPP
 
 #include "Task.hpp"
-#include <queue>  //to include task
 #include <memory>
-using namespace std;
+#include <mutex>
+#include <atomic>
+#include <semaphore.h>
+
 class ProcessManagement
 {
-    public:
-        ProcessManagement();
-        bool submitToQueue(unique_ptr<Task> task);
-        void executeTasks();
+    sem_t* itemsSemaphore; // semaphore to signal that there are items in the queue
+    sem_t* emptySlotsSemaphore; // semaphore to signal that there are empty slots in the queue
 
-    private:
-        queue<unique_ptr<Task>> taskQueue;
+public:
+    ProcessManagement();
+    ~ProcessManagement(); //free share memory
+    bool submitToQueue(std::unique_ptr<Task> task);
+    void executeTask();
+
+private:
+    struct SharedMemory {
+        std::atomic<int> size;
+        char tasks[1000][256];
+        int front;
+        int rear;
+
+        void printSharedMemory() {
+            std::cout<<size<<std::endl;
+            std::cout<<front<<std::endl;
+            std::cout<<rear<<std::endl;
+        }
+
+    };
+    SharedMemory* sharedMem;
+    int shmFd;
+    const char* SHM_NAME = "/my_queue";
+    std::mutex queueLock; // mutex to protect the shared memory
 };
 
 #endif
